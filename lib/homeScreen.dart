4d3076e0da4e73da3cc6bbaadbe2be49af849df7
Anil Raj <Anilrajpallana@gmail.com>
2022-11-03 23:04:52 +0530

    Completed

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'constants.dart' as k;
+import 'dart:convert';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,11 +13,12 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   bool isLoaded = false;
-  late num temp;
-  late num pressure;
-  late num hue;
-  late num cover;
-  late String city;
+  num ?temp;
+  num ?pressure;
+   num ?hum;
+  num ?cover;
+  late String cityname = '';
+  TextEditingController controller = TextEditingController();
 
   @override
   void initState() {
@@ -27,7 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
+        child: Scaffold(resizeToAvoidBottomInset: false,
       body: Container(
         width: double.infinity,
         height: double.infinity,
@@ -40,10 +42,208 @@ class _HomeScreenState extends State<HomeScreen> {
         ], begin: Alignment.bottomLeft, end: FractionalOffset.topRight)),
         child: Visibility(
           visible: isLoaded,
+          replacement: Center(
+            child: CircularProgressIndicator(),
+          ),
           child: Column(
-          ),replacement: Center(
-          child: ,
-        ),
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.85,
+                height: MediaQuery.of(context).size.height * 0.08,
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.3),
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: Center(
+                  child: TextFormField(
+                    onFieldSubmitted: (String s) {
+                      setState(() {
+                        cityname = s;
+                        getCityWeather(s);
+                        isLoaded = false;
+                        controller.clear();
+                      });
+                    },
+                    controller: controller,
+                    cursorColor: Colors.white,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white),
+                    decoration: InputDecoration(
+                        hintText: "Search City",
+                        hintStyle: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white.withOpacity(0.7)),
+                        prefixIcon: Icon(
+                          Icons.search_rounded,
+                          size: 25,
+                          color: Colors.white.withOpacity(0.7),
+                        ),
+                        border: InputBorder.none),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Icon(
+                      Icons.pin_drop,
+                      color: Colors.red,
+                      size: 40,
+                    ),
+                    Text(
+                      cityname,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.12,
+                margin: EdgeInsets.symmetric(vertical: 10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.shade900,
+                          offset: Offset(1, 2),
+                          blurRadius: 3,
+                          spreadRadius: 1)
+                    ]),
+                child: Row(
+                  children: [
+                    Image(
+                        image: AssetImage("images/thermometer.png"),
+                        width: MediaQuery.of(context).size.width * 0.09),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Temperature: ${temp?.toInt()} ℃",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.12,
+                margin: EdgeInsets.symmetric(vertical: 10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.shade900,
+                          offset: Offset(1, 2),
+                          blurRadius: 3,
+                          spreadRadius: 1)
+                    ]),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(
+                          image: AssetImage("images/barometer.png"),
+                          width: MediaQuery.of(context).size.width * 0.09),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Pressure: ${pressure?.toInt()} hPa",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.12,
+                margin: EdgeInsets.symmetric(vertical: 10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.shade900,
+                          offset: Offset(1, 2),
+                          blurRadius: 3,
+                          spreadRadius: 1)
+                    ]),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(
+                          image: AssetImage("images/humidity.png"),
+                          width: MediaQuery.of(context).size.width * 0.09),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Humidity: ${hum?.toInt()}%",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.12,
+                margin: EdgeInsets.symmetric(vertical: 10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.shade900,
+                          offset: Offset(1, 2),
+                          blurRadius: 3,
+                          spreadRadius: 1)
+                    ]),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(
+                          image: AssetImage("images/cloud cover.png"),
+                          width: MediaQuery.of(context).size.width * 0.09),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Cloud Cover: ${cover?.toInt()}%",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     ));
@@ -61,6 +261,24 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  getCityWeather(String cityname) async {
+    var client = http.Client();
+    var uri = "${k.domain}q=$cityname&appid=${k.apiKey}";
+    var url = Uri.parse(uri);
+    var response = await client.get(url);
+    if (response.statusCode == 200) {
+      var data = response.body;
+      var decodedData = json.decode(data);
+      // print(data);
+      updateUi(decodedData);
+      setState(() {
+        isLoaded = true;
+      });
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   getCurrentCityWeather(Position position) async {
     var client = http.Client();
     var uri =
@@ -69,9 +287,38 @@ class _HomeScreenState extends State<HomeScreen> {
     var response = await client.get(url);
     if (response.statusCode == 200) {
       var data = response.body;
+      var decodedData = json.decode(data);
       print(data);
+      updateUi(decodedData);
+      setState(() {
+        isLoaded = true;
+      });
     } else {
       print(response.statusCode);
     }
+  }
+
+  updateUi(var decodedData) {
+    setState(() {
+      if (decodedData == null) {
+        temp = 0;
+        pressure = 0;
+        cover = 0;
+        hum = 0;
+        cityname = "Not available";
+      } else {
+        temp = decodedData['main']['temp'] - 273;
+        pressure = decodedData['main']['pressure'];
+        cover = decodedData['clouds']['all'];
+        hum = decodedData['main']['humidity'];
+        cityname = decodedData['name'];
+      }
+    });
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  controller.dispose();
   }
 }
